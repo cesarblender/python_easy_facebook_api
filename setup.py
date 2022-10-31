4bce2c5538d747e2a840177bd0b393cae8809e6d
seokkuuu <seokkuuu>
2022-10-30 22:04:37 -0400

    released version 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 PACKAGE_NAME = 'python_easy_facebook_api'
 AUTHOR = 'Seokku'
 AUTHOR_EMAIL = 'seokkub@gmail.com'
